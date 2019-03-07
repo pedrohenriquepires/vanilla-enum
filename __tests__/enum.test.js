8680e5a2f5d169c86d0a24885ee242ea4e660386
Pedro Henrique Pires <pdrhenriquepires@gmail.com>
2019-03-07 14:40:29 -0300

    refactoring tests

--- __tests__/enum.test.js
+++ __tests__/enum.test.js
@@ -13,38 +13,29 @@ const GENDER = new Enum({
 
 describe('Enum', () => {
   it('getKeys must return ["Male", "Female"]', () => {
-    const keys = GENDER.getKeys()
-
-    expect(keys).toEqual(["Male", "Female"])
+    expect(GENDER.getKeys()).toEqual(["Male", "Female"])
   })
 
   it('female gender raw value must be 2', () => {
-    const female = GENDER.Female
-
-    expect(female).toBe(2)
+    expect(GENDER.Female).toBe(2)
   })
 
   it('male gender toString must be "Male"', () => {
-    const male = GENDER.Male.toString()
-
-    expect(male).toBe("Male")
+    expect(GENDER.Male.toString()).toBe("Male")
   })
 
   it('male gender is not equals to "MALE"', () => {
-    const male = GENDER.Male
-
-    expect(male.is("MALE")).toBe(false)
+    expect(GENDER.Male.is("MALE")).toBe(false)
   })
 
-  it('male gender is equals to "MALE" when ignoringCase', () => {
+  it('male gender is equals to "MALE" when ignorCase is true', () => {
     const GENDER = new Enum({
       Male: {
         value: 1,
         description: "Male"
       }
     }, { ignoreCase: true })
-    const male = GENDER.Male
 
-    expect(male.is("MALE")).toBe(true)
+    expect(GENDER.Male.is("MALE")).toBe(true)
   })
 })
